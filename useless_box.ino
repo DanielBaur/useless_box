963841f5843541fbe09c8c10d000bb27607ace7c
Daniel Baur <daniel@baurclan.de>
2021-10-12 02:08:53 +0200

    20211012: added useless profiles

--- useless_box.ino
+++ useless_box.ino
@@ -52,16 +52,20 @@ int servo_lid_endpos = 90;
 // switch servo
 Servo servo_switch; // erstellt ein Servo-Objekt um einen Servomotor zu steuern
 int servo_switch_startpos = 180;
+int servo_switch_midpos = 90;
+int servo_switch_beforealmostendpos = 35;
+int servo_switch_almostendpos = 15;
 int servo_switch_endpos = 0;
 
 
 // controlling the servos
-void move_servo_to(Servo controlservo, int endpos, int time_s) {
+void move_servo_to(Servo controlservo, int endpos, float time_s) {
   //void move_servo_to(int endpos, int time_s){
   //int currentpos = controlservo.read();
   int currentpos = controlservo.read();
-  int stepsize_servo = 10;
-  float stepsize_ms = (1000 * time_s) / (stepsize_servo * abs(endpos - currentpos));
+  int stepsize_servo = 1;
+  float steps_required = abs(endpos - currentpos)/stepsize_servo;
+  float stepsize_ms = (1000 * time_s) / steps_required;
   if (endpos > currentpos) {
     for (int pos = currentpos; pos <= endpos; pos += stepsize_servo) {
       controlservo.write(pos);
@@ -91,15 +95,100 @@ void move_servo_to(Servo controlservo, int endpos, int time_s) {
 // --------------------------------------------------
 
 
-void useless_action_1_standard() {
+// standard
+void useless_action_1() {
   delay(1500);
-  move_servo_to(servo_lid, servo_lid_endpos, 1);
+  move_servo_to(servo_lid, servo_lid_endpos, 0.5);
   delay(1500);
   move_servo_to(servo_switch, servo_switch_endpos, 1);
   delay(2000);
   move_servo_to(servo_switch, servo_switch_startpos, 1);
   delay(500);
   move_servo_to(servo_lid, servo_switch_startpos, 1);
+  delay(1000);
+}
+
+
+// stopping midway, toggling switch, returning
+void useless_action_2() {
+  delay(1000);
+  move_servo_to(servo_lid, servo_lid_endpos, 1);
+  delay(1500);
+  move_servo_to(servo_switch, servo_switch_midpos, 0.5);
+  delay(2000);
+  move_servo_to(servo_switch, servo_switch_endpos, 0.5);
+  delay(500);
+  move_servo_to(servo_switch, servo_switch_startpos, 0.5);
+  delay(500);
+  move_servo_to(servo_lid, servo_switch_startpos, 1);
+  delay(1000);
+}
+
+
+// dribbling, switching off, returning
+void useless_action_3() {
+  delay(1000);
+  move_servo_to(servo_lid, servo_lid_endpos, 0.25);
+  delay(500);
+  move_servo_to(servo_switch, servo_switch_almostendpos, 0.25); // 15
+  delay(40);
+  move_servo_to(servo_switch, servo_switch_beforealmostendpos, 0.25); // 35
+  delay(40);
+  move_servo_to(servo_switch, servo_switch_almostendpos, 0.25);
+  delay(40);
+  move_servo_to(servo_switch, servo_switch_beforealmostendpos, 0.25);
+  delay(40);
+  move_servo_to(servo_switch, servo_switch_almostendpos, 0.25);
+  delay(40);
+  move_servo_to(servo_switch, servo_switch_beforealmostendpos, 0.25);
+  delay(40);
+  move_servo_to(servo_switch, servo_switch_endpos, 0.15);
+  delay(500);
+  move_servo_to(servo_switch, servo_switch_startpos, 0.25);
+  delay(500);
+  move_servo_to(servo_lid, servo_switch_startpos, 1);
+  delay(1000);
+}
+
+
+// switching off and returning
+void useless_action_4() {
+  delay(1000);
+  move_servo_to(servo_lid, servo_lid_endpos, 0.25);
+  delay(100);
+  move_servo_to(servo_switch, servo_switch_endpos, 0.45);
+  delay(40);
+  move_servo_to(servo_switch, servo_switch_startpos, 0.45);
+  delay(40);
+  move_servo_to(servo_lid, servo_lid_startpos, 0.25);
+  delay(1200);
+  move_servo_to(servo_lid, servo_lid_endpos, 0.25);
+  delay(40);
+  move_servo_to(servo_switch, servo_switch_midpos, 0.25);
+  delay(40);
+  move_servo_to(servo_switch, servo_switch_startpos, 0.25);
+  delay(40);
+  move_servo_to(servo_lid, servo_lid_startpos, 0.25);
+  delay(1000);
+}
+
+
+// trying but failing, retrying and returning
+void useless_action_5() {
+  delay(1000);
+  move_servo_to(servo_lid, servo_lid_endpos, 0.25);
+  delay(100);
+  move_servo_to(servo_switch, servo_switch_almostendpos, 0.25);
+  delay(40);
+  move_servo_to(servo_switch, servo_switch_startpos, 0.45);
+  delay(1200);
+  move_servo_to(servo_switch, servo_switch_almostendpos, 0.75);
+  delay(250);
+  move_servo_to(servo_switch, servo_switch_endpos, 0.15);
+  delay(40);
+  move_servo_to(servo_switch, servo_switch_startpos, 0.45);
+  delay(40);
+  move_servo_to(servo_lid, servo_lid_startpos, 0.25);
   delay(1000);
 }
 
@@ -147,10 +236,24 @@ void loop() {
     Serial.print(random_useless_action);
     // animation
     if (random_useless_action == 1) {
-      //Serial.print("randac: ");
-      //Serial.print(random_useless_action);
       digitalWrite(led_pin, HIGH);
-      useless_action_1_standard();
+      useless_action_5();
+    }
+    else if (random_useless_action == 2) {
+      digitalWrite(led_pin, HIGH);
+      useless_action_2();
+    }
+    else if (random_useless_action == 3) {
+      digitalWrite(led_pin, HIGH);
+      useless_action_3();
+    }
+    else if (random_useless_action == 4) {
+      digitalWrite(led_pin, HIGH);
+      useless_action_4();
+    }
+    else if (random_useless_action == 5) {
+      digitalWrite(led_pin, HIGH);
+      useless_action_5();
     }
   }
 }
